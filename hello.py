ddebc5d298af8f6b4001a31a5a74c0fdf0e65da4
jkdoandeptrai <maidoan2002ns@gmail.com>
2021-10-11 17:06:29 +0700

    functions

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 hello = "Hello!"
-print("hello")
 # I changed this file at 4:55
+def helloWorld(x:int):
+    if not x:
+        print("hello world")
+    else:
+        print("hello gitHub")
+helloWorld(0)
